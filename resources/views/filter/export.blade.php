99169bb6ac3410f18ac59f58d6dae017a718552e
eye1905 <cfcyoga@gmail.com>
2022-10-17 10:53:29 +0700

    update report lagi kak

--- resources/views/filter/export.blade.php
+++ resources/views/filter/export.blade.php
@@ -1,19 +1,21 @@
- <button class="btn btn-sm btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Export</button>
-  <div class="dropdown-menu">
-    
-    @php
-    $furl = str_replace("report", "cetak", url()->current());
-    $params = request()->query();
-    $excel = "?format=excel";
-    $pdf = "?format=pdf";
-    $f_params = null;
-    foreach($params as $key => $value){
-        $f_params .= "&".$key."=".$value;
-    }
-    $excelurl = $furl.$excel.$f_params;
-    $pdfurl = $furl.$pdf.$f_params;
-    @endphp
-    
-    <a class="dropdown-item" target="_blank" href="{{ url($pdfurl) }}">PDF</a>
-    <a class="dropdown-item" target="_blank" href="{{ url($excelurl) }}">Excel</a>
-  </div>
+@if(StringHelper::getAccess("export_permission")==true)
+<button class="btn btn-sm btn-info dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Export</button>
+<div class="dropdown-menu">
+
+  @php
+  $furl = str_replace("report", "cetak", url()->current());
+  $params = request()->query();
+  $excel = "?format=excel";
+  $pdf = "?format=pdf";
+  $f_params = null;
+  foreach($params as $key => $value){
+    $f_params .= "&".$key."=".$value;
+  }
+  $excelurl = $furl.$excel.$f_params;
+  $pdfurl = $furl.$pdf.$f_params;
+  @endphp
+
+  <a class="dropdown-item" target="_blank" href="{{ url($pdfurl) }}">PDF</a>
+  <a class="dropdown-item" target="_blank" href="{{ url($excelurl) }}">Excel</a>
+</div>
+@endif
